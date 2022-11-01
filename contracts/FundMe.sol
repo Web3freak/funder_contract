8eefd66205d3361dd28819ef43d94f44fc80c1f7
Remix Workspace <>
2022-10-31 17:44:41 -0700

    Funder_contract

---
+++ contracts/FundMe.sol
@@ -0,0 +1,77 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+     
+    uint256 public constant MINIMUM_USD = 50 * 1e18; // 1 * 10 ** 18
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor(){
+       i_owner = msg.sender;
+
+    }
+
+    function fund() public payable{
+       require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough!");
+       funders.push(msg.sender);
+       addressToAmountFunded[msg.sender] += msg.value;
+
+    }
+
+    
+    function withdraw() public onlyOwner {
+        /* starting index, ending index, step amount */ 
+       for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex++){
+
+          address funder = funders[funderIndex];
+          addressToAmountFunded[funder] = 0;
+
+       }
+       //Reset array
+       funders = new address[](0);
+       //Withdraw the funds
+
+       // transfer
+       // send
+       // call
+       
+       // msg.sender = address
+       // payable(msg.sender) = payable address
+      payable(msg.sender).transfer(address(this).balance);
+      //bool sendSuccess = payable(msg.sender).send(address(this).balance);
+      //require(sendSuccess, "Send failed");
+      (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+      require(callSuccess, "Call failed");
+
+    }
+
+    modifier onlyOwner {
+       require(msg.sender == i_owner, "Sender is not owner");
+       _;
+    }
+   
+    receive() external payable{
+       fund();
+    }
+
+    fallback() external payable{
+       fund();
+    }
+
+}
+
+
+
+   
